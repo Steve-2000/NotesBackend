ae8cc4f2c71b1862614bfe7205ad9aa772ff4202
Jeganathan Dilakshan <153415262+Steve-2000@users.noreply.github.com>
2026-02-11 19:17:24 +0530

    Enable CORS by adding cors middleware

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require("dotenv").config();
 const app = express();
-// const cors=require("cors")
+const cors=require("cors")
 app.use(
   cors({
     origin: [
